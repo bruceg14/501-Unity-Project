bfac99218176177393c1d292c91309f3e02a147f
bruceg14 <ziyuan.guo@vanderbilt.edu>
2025-04-02 00:03:44 -0600

    Control Flow Graph for If statement

---
+++ cs/ControlFlowGraph/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.FlowAnalysis;
+
+class CFG_Node {
+    public int id;
+    public string content;
+}
+
+class CFG_Edge {
+    public CFG_Node source;
+    public CFG_Node target;
+}
+
+class CFG_Graph {
+    public List<CFG_Node> nodes;
+    public List<CFG_Edge> edges;
+
+    public CFG_Graph() {
+        nodes = new List<CFG_Node>();
+        edges = new List<CFG_Edge>();
+    }
+
+    public CFG_Graph(List<CFG_Node> nodes, List<CFG_Edge> edges) {
+        this.nodes = nodes;
+        this.edges = edges;
+    }
+
+    public void AddNode(CFG_Node node) {
+        nodes.Add(node);
+    }
+
+    public void AddEdge(CFG_Edge edge) {
+        edges.Add(edge);
+    }
+
+    public void PrintGraph() {
+        foreach (var node in nodes) {
+            Console.WriteLine($"Node {node.id}: {node.content}");
+        }
+        foreach (var edge in edges) {
+            Console.WriteLine($"Edge from {edge.source.id} to {edge.target.id}");
+        }
+    }
+}
+
+
+class CFG{
+    public CFG_Graph graph = new CFG_Graph();
+
+    public CFG_Node RecursiveCFGCreation(SyntaxList<StatementSyntax> statements, CFG_Node node) {
+        if(statements.Count == 0) {
+            return null;
+        }
+        foreach (var statement in statements) {
+            if (statement is IfStatementSyntax) {
+                var ifNode = new CFG_Node { id = graph.nodes.Count, content = "If statement" };
+                graph.AddNode(ifNode);
+                graph.AddEdge(new CFG_Edge { source = node, target = ifNode });
+                
+                var ifStatement = (IfStatementSyntax)statement;
+
+                // Check the then part of the if statement
+                var thenBlock = ifStatement.Statement as BlockSyntax;
+                var thenNode = new CFG_Node { id = graph.nodes.Count, content = "Then block" };
+                graph.AddNode(thenNode);
+                graph.AddEdge(new CFG_Edge { source = ifNode, target = thenNode });
+
+                var afterIfNode = new CFG_Node { id = graph.nodes.Count, content = "After if" };
+                graph.AddNode(afterIfNode);
+
+                Console.WriteLine($"Then block: {thenBlock}");
+                // Create nodes for the then and else blocks
+                if(thenBlock.Statements.Count > 0) {
+                    var exitThenNode = RecursiveCFGCreation(thenBlock.Statements, thenNode); 
+                    if (exitThenNode != null) {
+                        graph.AddEdge(new CFG_Edge { source = exitThenNode, target = afterIfNode });
+                    }
+                }
+
+                // Check the else part of the if statement
+                var elseBlock = ifStatement.Else?.Statement as BlockSyntax;
+                if(elseBlock!= null && elseBlock.Statements.Count > 0) {
+                    var elseNode = new CFG_Node { id = graph.nodes.Count, content = "Else block" };
+                    graph.AddNode(elseNode);
+                    graph.AddEdge(new CFG_Edge { source = ifNode, target = elseNode });
+
+                    var exitElseNode = RecursiveCFGCreation(elseBlock.Statements, elseNode);
+                    graph.AddEdge(new CFG_Edge { source = exitElseNode, target = afterIfNode });
+                    
+                } else {
+                    graph.AddEdge(new CFG_Edge { source = ifNode, target = afterIfNode });
+                }
+
+                node = afterIfNode;
+            }
+            
+        }
+
+        return node;
+    }
+
+    public static void Main(string[] args)
+    {
+        var sourceCode = File.ReadAllText("Test.cs");
+        var syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
+        var root = syntaxTree.GetRoot();
+        
+        var methods = root.DescendantNodes().OfType<MethodDeclarationSyntax>();
+        foreach (var method in methods)
+        {   
+            var controlFlowGraph = new CFG();
+            var entryNode = new CFG_Node { id = 0, content = "Entry" };
+            controlFlowGraph.graph.AddNode(entryNode);
+            controlFlowGraph.RecursiveCFGCreation(method.Body.Statements, entryNode);
+            controlFlowGraph.graph.PrintGraph();
+        }
+    }
+}
---
+++ cs/ControlFlowGraph/Test.cs
@@ -0,0 +1,24 @@
+class Test {
+    void ifTest() {
+        int x = 1;
+        if (x == 1) {
+            if (x > 0) {
+                x = 1;
+            }
+            x = 2;
+        } else {
+            x = 3;
+        }
+    }
+
+    // void ifTest1() {
+    //     int x = 1;
+    //     if (x == 1) {
+    //         x += 1;
+    //     } else {
+    //         x = 3;
+    //     }
+    // }
+
+
+}
