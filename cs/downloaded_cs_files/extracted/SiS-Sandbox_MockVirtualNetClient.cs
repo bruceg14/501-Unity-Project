6b908a163701f13f44d8aeaf0dd4815bbaf9b52f
bruceg14 <ziyuan.guo@vanderbilt.edu>
2025-04-21 20:35:24 -0600

    Clean up

--- cs/downloaded_cs_files/extracted/SiS-Sandbox_MockVirtualNetClient.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Game.Networking
-{
-    public class MockVirtualNetClient : VirtualNetClient
-    {
-        public int BadProtocolPackets { get { return m_Stats.BadProtocolPackets; } }
-        public int CorruptAckPackets { get { return m_Stats.CorruptAckPackets; } }
-    }
-}
