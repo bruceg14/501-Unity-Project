06c08f457d78d3262442acacfd7ebc6e6753916c
bruceg14 <ziyuan.guo@vanderbilt.edu>
2025-04-21 23:43:31 -0600

    Original CS files before translation

---
+++ cs/ControlFlowGraph/original_files/Beroepsopdracht-ArcadeMechanics_MovementChar.cs
@@ -0,0 +1,453 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementChar : MonoBehaviour
+{
+    
+    //this bool is for the ability to move the char
+    public bool AbleToMove = true;
+    public static float SpeedGameChar = 4f;
+
+    public bool GameCharHasNoKey = true;
+    public bool GameCharHasBlueKey = false;
+    public bool GameCharHasGreenKey = false;
+    public bool GameCharHasYellowKey = false;
+    public bool GameCharHasRedKey = false;
+    public bool GameCharHasPinkKey = false;
+
+    public bool GameCharHasNoKeyHat = false;
+    public bool GameCharHasBlueKeyHat = false;
+    public bool GameCharHasGreenKeyHat = false;
+    public bool GameCharHasYellowKeyHat = false;
+    public bool GameCharHasRedKeyHat = false;
+    public bool GameCharHasPinkKeyHat = false;
+
+    public GameObject Up;
+    public GameObject UpKey;
+    public GameObject UpHat;
+    public GameObject UpHatKey;
+
+
+    public GameObject Down;
+    public GameObject DownBlueKey;
+    public GameObject DownGreenKey;
+    public GameObject DownPinkKey;
+    public GameObject DownRedKey;
+    public GameObject DownYellowKey;
+
+    public GameObject DownHat;
+    public GameObject DownBlueKeyHat;
+    public GameObject DownGreenKeyHat;
+    public GameObject DownPinkKeyHat;
+    public GameObject DownRedKeyHat;
+    public GameObject DownYellowKeyHat;
+
+    public GameObject Right;
+
+    public GameObject Left;
+
+    // called once every frame
+    void FixedUpdate()
+    {
+        
+        if(AbleToMove)
+        {
+            if (GameCharHasNoKey)
+            {
+                //if W is pressed GameChar goes up
+                if (Input.GetKey(KeyCode.W))
+                {
+                    transform.position += new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                    Up.SetActive(true);
+                    Down.SetActive(false);
+                    Right.SetActive(false);
+                    Left.SetActive(false);
+                }
+
+                //if S is pressed GameChar goes down
+                if (Input.GetKey(KeyCode.S))
+                {
+                    transform.position -= new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+
+                    Up.SetActive(false);
+                    Down.SetActive(true);
+                    Right.SetActive(false);
+                    Left.SetActive(false);
+                }
+
+                //if D is pressed GameChar goes to the Left
+                if (Input.GetKey(KeyCode.D))
+                {
+                    transform.position += new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+
+                    
+                    Up.SetActive(false);
+                    Down.SetActive(false);
+                    Right.SetActive(true);
+                    Left.SetActive(false);
+                    
+                }
+
+                //if A is pressed GameChar goes to the right
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.position -= new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+
+                    Up.SetActive(false);
+                    Down.SetActive(false);
+                    Right.SetActive(false);
+                    Left.SetActive(true);
+                }
+            }
+
+            if (GameCharHasBlueKey)
+            {
+                //if W is pressed GameChar goes up
+                if (Input.GetKey(KeyCode.W))
+                {
+                    transform.position += new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if S is pressed GameChar goes down
+                if (Input.GetKey(KeyCode.S))
+                {
+                    transform.position -= new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if D is pressed GameChar goes to the Left
+                if (Input.GetKey(KeyCode.D))
+                {
+                    transform.position += new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                }
+
+                //if A is pressed GameChar goes to the right
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.position -= new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                    Up.SetActive(false);
+                    Down.SetActive(false);
+                    Right.SetActive(false);
+                    Left.SetActive(true);
+                }
+            }
+
+            if (GameCharHasGreenKey)
+            {
+                //if W is pressed GameChar goes up
+                if (Input.GetKey(KeyCode.W))
+                {
+                    transform.position += new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if S is pressed GameChar goes down
+                if (Input.GetKey(KeyCode.S))
+                {
+                    transform.position -= new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if D is pressed GameChar goes to the Left
+                if (Input.GetKey(KeyCode.D))
+                {
+                    transform.position += new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                }
+
+                //if A is pressed GameChar goes to the right
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.position -= new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                    Up.SetActive(false);
+                    Down.SetActive(false);
+                    Right.SetActive(false);
+                    Left.SetActive(true);
+                }
+            }
+
+            if (GameCharHasYellowKey)
+            {
+                //if W is pressed GameChar goes up
+                if (Input.GetKey(KeyCode.W))
+                {
+                    transform.position += new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if S is pressed GameChar goes down
+                if (Input.GetKey(KeyCode.S))
+                {
+                    transform.position -= new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if D is pressed GameChar goes to the Left
+                if (Input.GetKey(KeyCode.D))
+                {
+                    transform.position += new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                }
+
+                //if A is pressed GameChar goes to the right
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.position -= new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                    Up.SetActive(false);
+                    Down.SetActive(false);
+                    Right.SetActive(false);
+                    Left.SetActive(true);
+                }
+            }
+
+            if (GameCharHasRedKey)
+            {
+                //if W is pressed GameChar goes up
+                if (Input.GetKey(KeyCode.W))
+                {
+                    transform.position += new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if S is pressed GameChar goes down
+                if (Input.GetKey(KeyCode.S))
+                {
+                    transform.position -= new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if D is pressed GameChar goes to the Left
+                if (Input.GetKey(KeyCode.D))
+                {
+                    transform.position += new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                }
+
+                //if A is pressed GameChar goes to the right
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.position -= new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                    Up.SetActive(false);
+                    Down.SetActive(false);
+                    Right.SetActive(false);
+                    Left.SetActive(true);
+                }
+            }
+
+            if (GameCharHasPinkKey)
+            {
+                //if W is pressed GameChar goes up
+                if (Input.GetKey(KeyCode.W))
+                {
+                    transform.position += new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if S is pressed GameChar goes down
+                if (Input.GetKey(KeyCode.S))
+                {
+                    transform.position -= new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if D is pressed GameChar goes to the Left
+                if (Input.GetKey(KeyCode.D))
+                {
+                    transform.position += new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                }
+
+                //if A is pressed GameChar goes to the right
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.position -= new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                    Up.SetActive(false);
+                    Down.SetActive(false);
+                    Right.SetActive(false);
+                    Left.SetActive(true);
+                }
+            }
+
+            if (GameCharHasNoKeyHat)
+            {
+                //if W is pressed GameChar goes up
+                if (Input.GetKey(KeyCode.W))
+                {
+                    transform.position += new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if S is pressed GameChar goes down
+                if (Input.GetKey(KeyCode.S))
+                {
+                    transform.position -= new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if D is pressed GameChar goes to the Left
+                if (Input.GetKey(KeyCode.D))
+                {
+                    transform.position += new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                }
+
+                //if A is pressed GameChar goes to the right
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.position -= new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                    Up.SetActive(false);
+                    Down.SetActive(false);
+                    Right.SetActive(false);
+                    Left.SetActive(true);
+                }
+            }
+
+            if (GameCharHasBlueKeyHat)
+            {
+                //if W is pressed GameChar goes up
+                if (Input.GetKey(KeyCode.W))
+                {
+                    transform.position += new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if S is pressed GameChar goes down
+                if (Input.GetKey(KeyCode.S))
+                {
+                    transform.position -= new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if D is pressed GameChar goes to the Left
+                if (Input.GetKey(KeyCode.D))
+                {
+                    transform.position += new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                }
+
+                //if A is pressed GameChar goes to the right
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.position -= new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                    Up.SetActive(false);
+                    Down.SetActive(false);
+                    Right.SetActive(false);
+                    Left.SetActive(true);
+                }
+            }
+
+            if (GameCharHasGreenKeyHat)
+            {
+                //if W is pressed GameChar goes up
+                if (Input.GetKey(KeyCode.W))
+                {
+                    transform.position += new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if S is pressed GameChar goes down
+                if (Input.GetKey(KeyCode.S))
+                {
+                    transform.position -= new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if D is pressed GameChar goes to the Left
+                if (Input.GetKey(KeyCode.D))
+                {
+                    transform.position += new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                }
+
+                //if A is pressed GameChar goes to the right
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.position -= new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                    Up.SetActive(false);
+                    Down.SetActive(false);
+                    Right.SetActive(false);
+                    Left.SetActive(true);
+                }
+            }
+
+            if (GameCharHasYellowKeyHat)
+            {
+                //if W is pressed GameChar goes up
+                if (Input.GetKey(KeyCode.W))
+                {
+                    transform.position += new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if S is pressed GameChar goes down
+                if (Input.GetKey(KeyCode.S))
+                {
+                    transform.position -= new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if D is pressed GameChar goes to the Left
+                if (Input.GetKey(KeyCode.D))
+                {
+                    transform.position += new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                }
+
+                //if A is pressed GameChar goes to the right
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.position -= new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                    Up.SetActive(false);
+                    Down.SetActive(false);
+                    Right.SetActive(false);
+                    Left.SetActive(true);
+                }
+            }
+
+            if (GameCharHasRedKeyHat)
+            {
+                //if W is pressed GameChar goes up
+                if (Input.GetKey(KeyCode.W))
+                {
+                    transform.position += new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if S is pressed GameChar goes down
+                if (Input.GetKey(KeyCode.S))
+                {
+                    transform.position -= new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if D is pressed GameChar goes to the Left
+                if (Input.GetKey(KeyCode.D))
+                {
+                    transform.position += new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                }
+
+                //if A is pressed GameChar goes to the right
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.position -= new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                    Up.SetActive(false);
+                    Down.SetActive(false);
+                    Right.SetActive(false);
+                    Left.SetActive(true);
+                }
+            }
+
+            if (GameCharHasPinkKeyHat)
+            {
+                //if W is pressed GameChar goes up
+                if (Input.GetKey(KeyCode.W))
+                {
+                    transform.position += new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if S is pressed GameChar goes down
+                if (Input.GetKey(KeyCode.S))
+                {
+                    transform.position -= new Vector3(0, SpeedGameChar, 0) * Time.deltaTime;
+                }
+
+                //if D is pressed GameChar goes to the Left
+                if (Input.GetKey(KeyCode.D))
+                {
+                    transform.position += new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                }
+
+                //if A is pressed GameChar goes to the right
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.position -= new Vector3(SpeedGameChar, 0, 0) * Time.deltaTime;
+                    Up.SetActive(false);
+                    Down.SetActive(false);
+                    Right.SetActive(false);
+                    Left.SetActive(true);
+                }
+            }
+
+
+            
+        }
+        
+    }
+    
+
+
+}
---
+++ cs/ControlFlowGraph/original_files/arkanoidclone_BrickBase.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public abstract class BrickBase : MonoBehaviour, IGolpeable
+{
+    private Transform _transform;
+    [SerializeField]
+    private BrickType _type;
+    private BlockSocket _socket;
+    private bool _canBeDestroyed = false;
+    private BoxCollider2D _brickCollider = null;
+    private Sprite _brickSprite = null;
+    [SerializeField]
+    private int _hits = 1;
+    private int _hitCount = 0;
+    public BrickType Type { get => _type; set => _type = value; }
+    public BlockSocket Socket { get => _socket; set => _socket = value; }
+    public bool CanBeDestroyed { get => _canBeDestroyed; set => _canBeDestroyed = value; }
+    public Sprite BrickSprite { get => _brickSprite; set => _brickSprite = value; }
+    public int Hits { get => _hits; }
+
+    public BrickBase(BlockSocket socket)
+    {
+        _socket = socket;
+    }
+
+    public virtual void Start()
+    {
+        _transform = transform;
+        _socket = _transform.GetComponentInParent<BlockSocket>();
+        _brickCollider = _transform.GetComponent<BoxCollider2D>();
+    }
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetDamage(int damage)
+    {
+        _hitCount += damage;
+
+        if (_hitCount >= _hits)
+            DestroyBrick();
+            
+    }
+
+    public void SetBrick()
+    {
+        if(transform.parent != null)
+            transform.position = transform.parent.position;
+
+        switch((int)Type)
+        {
+           
+            case (0):
+                
+                _hits = 1;
+
+                if ((int)Type != (int)BrickType.Brown)
+                    CanBeDestroyed = true;
+                else
+                    CanBeDestroyed = false;
+                //CanBeDestroyed = (int)Type == (int)BrickType.Brown ? true : false;
+
+                _socket.CurrentBrickType = (int)Type;
+
+                break;
+
+            case (1):
+
+                _hits = 2;
+
+                if ((int)Type != (int)BrickType.Brown)
+                    CanBeDestroyed = true;
+                else
+                    CanBeDestroyed = false;
+
+                _socket.CurrentBrickType = (int)Type;
+
+                break;
+
+            case (2):
+
+                _hits = 3;
+
+                if ((int)Type != (int)BrickType.Brown)
+                    CanBeDestroyed = true;
+                else
+                    CanBeDestroyed = false;
+
+                _socket.CurrentBrickType = (int)Type;
+
+                break;
+
+            case (3):
+                 
+                _hits = -1;
+
+                if ((int)Type != (int)BrickType.Brown)
+                    CanBeDestroyed = true;
+                else
+                    CanBeDestroyed = false;
+
+                _socket.CurrentBrickType = (int)Type;
+                
+                break;
+
+            default:
+                _hits = 1;
+                break;
+        }
+        
+    }
+
+    public void DisableBrick()
+    {
+        if (_hitCount >= _hits)
+            _transform.gameObject.SetActive(false);
+
+        //resetPosition
+    }
+
+    public void DestroyBrick()
+    {
+        Destroy(_transform.gameObject);
+        // Instantiate FireSprite/ lo que sea si hace falta
+        // calcular probabilidad de objeto y devolver Type para buscar :: item.Type == selectedItem
+        //Instantiate(GameManager.Instance.ItemPrefabs.SingleOrDefault(item => item.Type == "GreenGem"));
+    }
+
+
+}
